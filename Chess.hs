0fd49bdb0f18ba0ab72cc4be24ca465ba738a802
2023-08-21 <sdesai1@janus06.cs.trinity.edu>
2024-11-20 14:50:50 -0600

    Removed redundant initialGame

--- Chess.hs
+++ Chess.hs
@@ -232,21 +232,3 @@ toString game = unlines $ boardRows ++ [footer]
 
 printGame :: Game -> IO ()--written with the help of ChatGPT
 printGame game = putStrLn $ toString game
-
--- For GHCI testing
-initialGame :: Game-- Written with the help of ChatGPT
-initialGame = (White, whitePieces, blackPieces,100)--may need to change turn count
-  where
-    whitePawns   = [((x, 2), (Pawn, White)) | x <- [1..8]]
-    whitePieces = [
-        ((1, 1), (Rook, White)), ((2, 1), (Knight, White)), ((3, 1), (Bishop, White)),
-        ((4, 1), (Queen, White)), ((5, 1), (King, White)),
-        ((6, 1), (Bishop, White)), ((7, 1), (Knight, White)), ((8, 1), (Rook, White))
-      ] ++ whitePawns
-
-    blackPawns   = [((x, 7), (Pawn, Black)) | x <- [1..8]]
-    blackPieces = [
-        ((1, 8), (Rook, Black)), ((2, 8), (Knight, Black)), ((3, 8), (Bishop, Black)),
-        ((4, 8), (Queen, Black)), ((5, 8), (King, Black)),
-        ((6, 8), (Bishop, Black)), ((7, 8), (Knight, Black)), ((8, 8), (Rook, Black))
-      ] ++ blackPawns
