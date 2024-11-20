bd46b88ff19743d98c6f243c46a5484c480ac281
Brandon <braimond@trinity.edu>
2024-11-20 12:21:50 -0600

    need to test whoWillWin

--- ChessGPT.hs
+++ ChessGPT.hs
@@ -2,7 +2,7 @@ module ChessGPT where
 import Chess
 
 whoWillWin :: Game -> Winner--maybe use point system?https://www.chess.com/terms/chess-piece-value
-whoWillWin game = undefined
+whoWillWin game = ((material game) * 10) + (spaceControlled game) + (pawnStructure game) + ((kingSafety game) * 6)
 
 material :: Game -> Integer
 material (_,whites,blacks) = wMaterial - bMaterial
@@ -25,21 +25,42 @@ spaceControlled :: Game -> Integer
 spaceControlled (_,whites, blacks) = (length (possibleGameMoves (White,whites,blacks))) - (length (possibleGameMoves (Black,whites,blacks)))
 
 pawnStructure :: Game -> Integer
-pawnStructure game = 
-    where wPawns = [(pos, (type, team)) | (pos, (type, team)) <- whites, type == Pawn]
-          bPawns = [(pos, (type, team)) | (pos, (type, team)) <- whites, type == Pawn]
-          wStructure = length [x | x <- wPawns, ]--use adjacentTeamPieces
-          bStructure = 
+pawnStructure game = wStructure - bStructure
+    where (_, whites, blacks) = game
+          wPawns = [(pos, (type, team)) | (pos, (type, team)) <- whites, type == Pawn]
+          bPawns = [(pos, (type, team)) | (pos, (type, team)) <- blacks, type == Pawn]
+          leftWPawn (x,y) = (getPiece game (x-1,y+1)) == maybe ((x-1,y+1), (Pawn, White))
+          rightWPawn (x,y) = (getPiece game (x+1,y+1)) == maybe ((x+1,y+1), (Pawn, White))
+          wStructure = sum [n | ((x,y) (_,_)) <- wPawns, if leftWPawn (x,y) && rightWPawn (x,y) then n = 2
+                                                         else if leftWPawn (x,y) || rightWPawn (x,y) then n = 1
+                                                         else n = 0]
+          leftBPawn (x,y) = (getPiece game (x-1,y-1)) == maybe ((x-1,y-1), (Pawn, Black))
+          rightBPawn (x,y) = (getPiece game (x+1,y-1)) == maybe ((x+1,y-1), (Pawn, Black))
+          bStructure = sum [n | ((x,y) (_,_)) <- bPawns, if leftBPawn (x,y) && rightBPawn (x,y) then n = 2
+                                                         else if leftBPawn (x,y) || rightBPawn (x,y) then n = 1
+                                                         else n = 0]
+          
 
-adjacentTeamPieces :: Game -> Piece -> [Pieces]
+{-adjacentTeamPieces :: Game -> Piece -> [Pieces]
 adjacentTeamPieces game piece = if getPieceTeam piece == white then length [pos | pos <- adjacentPositions, (getPiece game pos) /= null]--need to only check for one team somehow
                                 else 
                 where (pieceX,pieceY) = getPosition piece
                       adjacentPositions = [(pieceX-1, pieceY+1),(pieceX, pieceY+1),(pieceX+1, pieceY+1),(pieceX-1, pieceY)
-                                       ,(pieceX+1, pieceY),(pieceX-1, pieceY-1),(pieceX, pieceY-1),(pieceX+1, pieceY-1)]
+                                       ,(pieceX+1, pieceY),(pieceX-1, pieceY-1),(pieceX, pieceY-1),(pieceX+1, pieceY-1)]-}
 
-kingSafety :: Game -> Integer--just check how many friendly pieces are around the king for now using adjecentTeamPieces
-kingSafety game = undefined
+kingSafety :: Game -> Integer--at least 2 pawns in front of king
+kingSafety game = (if wKingDefense == 3 then 2 else wKingDefense) - (if bKingDefense == 3 then 2 else bKingDefense)
+    where (_, whites, blacks) = game
+          wKing = [(pos, (type, team)) | (pos, (type, team)) <- whites, type == King]
+          bKing = [(pos, (type, team)) | (pos, (type, team)) <- blacks, type == King]
+          leftWPawn (x,y) = if (getPiece game (x-1,y+1)) == maybe ((x-1,y+1), (Pawn, White)) then 1 else 0
+          middleWPawn (x,y) = if (getPiece game (x,y+1)) == maybe ((x,y+1), (Pawn, White)) then 1 else 0
+          rightWPawn (x,y) = if (getPiece game (x+1,y+1)) == maybe ((x+1,y+1), (Pawn, White)) then 1 else 0
+          wKingDefense = leftWPawn (getPosition wKing) + middleWPawn (getPosition wKing) + rightWPawn (getPosition wKing)
+          leftBPawn (x,y) = if (getPiece game (x-1,y-1)) == maybe ((x-1,y-1), (Pawn, Black)) then 1 else 0
+          middleBPawn (x,y) = if (getPiece game (x,y-1)) == maybe ((x,y-1), (Pawn, Black)) then 1 else 0
+          rightBPawn (x,y) = if (getPiece game (x+1,y-1)) == maybe ((x+1,y-1), (Pawn, Black)) then 1 else 0
+          bKingDefense = leftBPawn (getPosition bKing) + middleBPawn (getPosition bKing) + rightBPawn (getPosition bKing)
 
 
 
