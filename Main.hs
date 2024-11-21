384d774c9125baa53bda7eae95330e7417e4b041
Morgan Powers <morganpowers@jessicas-air-3.trinity.local>
2024-11-21 12:05:13 -0600

    main stuff

--- Main.hs
+++ Main.hs
@@ -2,15 +2,17 @@ module Main where
 import Chess
 import ChessGPT
 import Testing
+import System.IO.Unsafe
 
 writeGame :: Game -> FilePath -> IO ()
 writeGame game path = undefined
 
 loadGame :: FilePath -> IO Game 
-loadGame path = undefined
+loadGame path = putStrLn $ readGame str
+  where str = unsafePerformIO $ readFile path
 
 putBestMove :: Game -> IO ()
-putBestMove game = undefined
+putBestMove game = putStrLn $ show $ bestMove game
 
 --run with runhaskell Main.hs in command line
 main :: IO()
