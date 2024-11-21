b142b3d531f110d705f36ae3513aaed8f411b79b
Morgan Powers <morganpowers@jessicas-air-3.trinity.local>
2024-11-21 17:02:37 -0600

    loadGame + putBestMove

--- Main.hs
+++ Main.hs
@@ -2,14 +2,14 @@ module Main where
 import Chess
 import ChessGPT
 import Testing
-import System.IO.Unsafe
 
 writeGame :: Game -> FilePath -> IO ()
 writeGame game path = undefined
 
 loadGame :: FilePath -> IO Game 
-loadGame path = putStrLn $ readGame str
-  where str = unsafePerformIO $ readFile path
+loadGame path = do
+  str <- readFile path
+  return $ readGame str
 
 putBestMove :: Game -> IO ()
 putBestMove game = putStrLn $ show $ bestMove game
